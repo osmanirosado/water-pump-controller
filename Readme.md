ddd2cd39d5564a33326c29ace9d16fef3cd2f731
Osmani Rosado-Falcon <osmanir@uclv.cu>
2021-10-29 11:55:55 -0400

    Remove one touch sensor and user the other one to start/stop the system.

--- Readme.md
+++ Readme.md
@@ -17,17 +17,16 @@
 2. Conectar el led verde en el puerto D2.
 3. Conectar el led azul en el puerto D3.
 4. Conectar el led rojo en el puerto D4.
-5. Conectar dos botones de contacto (touch sensor) en los puertos D5 y D6.
+5. Conectar el botón de contacto (touch sensor) en el puerto D5.
 6. Conectar los relés (2-Channel SPDT Relay) en el puerto D7.  
    El puerto D8 es usado por uno de los reles conectados en el puerto D7.  
    Esto sucede porque el pin digital 8 está disponible también en el puerto D7.
 
 ## Comportamiento
 
-Hay dos botones de contacto, uno para encender la bomba de agua y el otro 
-para detenerla.
+Hay un botón de contacto, para encender y detener la bomba de agua.
 
-Al hacer contacto en el botón de inicio, 
+Al hacer contacto en el botón (cuando el sistema esta apagado), 
 empieza un proceso de bombeo y recuperación que se repite tres veces.
 Primero se inicia un temporizador que, detiene el bombeo después de un
 tiempo. Luego se inicia otro temporizador,
@@ -37,7 +36,8 @@ antes de iniciar el proceso nuevamente.
 El número de veces que se repite el proceso, el tiempo de bombeo
 y el tiempo de reposo, son constantes definidas en el código.
 
-Al hacer contacto en el botón de apagado, se detiene el proceso.
+Al hacer contacto en el botón (cuando el sistema esta encendido), 
+se detiene el proceso.
 Si la bomba está funcionando se apaga. Si el temporizador de recuperación
 está funcionando, se deja terminar para evitar un inicio apresurado.
 
--- sketch_sep29a/sketch_sep29a.ino
+++ sketch_sep29a/sketch_sep29a.ino
@@ -5,8 +5,7 @@
 #define RECOVERY_BLUE_LED 3
 #define IDLE_RED_LED 4
 
-#define START_BTN 5
-#define STOP_BTN 6
+#define START_STOP_BTN 5
 
 #define NO_RELAY 7
 #define NC_RELAY 8
@@ -23,68 +22,67 @@ Neotimer runTimer = Neotimer(RUN_TIME);
 Neotimer recoveryTimer = Neotimer(RECOVERY_TIME);
 
 void blinkRelay(uint8_t pin) {
-  digitalWrite(pin, HIGH);
-  delay(500);
-  digitalWrite(pin, LOW);
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
 }
 
 void startMotor() {
-  runTimer.reset();
-  runTimer.start();
-  digitalWrite(RECOVERY_BLUE_LED, LOW);
-  digitalWrite(IDLE_RED_LED, LOW);
-  digitalWrite(RUNNING_GREEN_LED, HIGH);
-  blinkRelay(NO_RELAY);
+    runTimer.reset();
+    runTimer.start();
+    digitalWrite(RECOVERY_BLUE_LED, LOW);
+    digitalWrite(IDLE_RED_LED, LOW);
+    digitalWrite(RUNNING_GREEN_LED, HIGH);
+    blinkRelay(NO_RELAY);
 }
 
 void stopMotor() {
-  blinkRelay(NC_RELAY);
-  digitalWrite(RUNNING_GREEN_LED, LOW);
-  recoveryTimer.reset();
-  recoveryTimer.start();
-  digitalWrite(RECOVERY_BLUE_LED, HIGH);
+    blinkRelay(NC_RELAY);
+    digitalWrite(RUNNING_GREEN_LED, LOW);
+    recoveryTimer.reset();
+    recoveryTimer.start();
+    digitalWrite(RECOVERY_BLUE_LED, HIGH);
 }
 
 void setup() {
-  pinMode(RUNNING_GREEN_LED, OUTPUT);
-  pinMode(RECOVERY_BLUE_LED, OUTPUT);
-  pinMode(IDLE_RED_LED, OUTPUT);
-  pinMode(START_BTN, INPUT);
-  pinMode(STOP_BTN, INPUT);
-  pinMode(NO_RELAY, OUTPUT);
-  pinMode(NC_RELAY, OUTPUT);
-  idle = true;
-  digitalWrite(IDLE_RED_LED, HIGH);
+    pinMode(RUNNING_GREEN_LED, OUTPUT);
+    pinMode(RECOVERY_BLUE_LED, OUTPUT);
+    pinMode(IDLE_RED_LED, OUTPUT);
+    pinMode(START_STOP_BTN, INPUT);
+    pinMode(NO_RELAY, OUTPUT);
+    pinMode(NC_RELAY, OUTPUT);
+    idle = true;
+    digitalWrite(IDLE_RED_LED, HIGH);
 }
 
 void loop() {
-  if (digitalRead(START_BTN) == HIGH && idle) {
-    runCount = 0;
-    runTotal = TIMES;
-    idle = false;
-    startMotor();
-  }
-  
-  if (digitalRead(STOP_BTN) == HIGH) {
-    runTotal = runCount;
-    if (runTimer.waiting()) {
-      runTimer.stop();
-      stopMotor();
+    if (digitalRead(START_STOP_BTN) == HIGH) {
+        if (idle) {
+            runCount = 0;
+            runTotal = TIMES;
+            idle = false;
+            startMotor();
+        } else {
+            runTotal = runCount;
+            if (runTimer.waiting()) {
+                runTimer.stop();
+                stopMotor();
+            }
+        }
     }
-  }
 
-  if (runTimer.done()) {
-    runCount++;
-    stopMotor();
-  }
-
-  if (recoveryTimer.done()) {
-    digitalWrite(RECOVERY_BLUE_LED, LOW);
-    if (runCount < runTotal) {
-      startMotor();  
-    } else {
-      idle = true;
-      digitalWrite(IDLE_RED_LED, HIGH);
+    if (runTimer.done()) {
+        runCount++;
+        stopMotor();
     }
-  }
+
+    if (recoveryTimer.done()) {
+        digitalWrite(RECOVERY_BLUE_LED, LOW);
+        if (runCount < runTotal) {
+            startMotor();
+        } else {
+            idle = true;
+            digitalWrite(IDLE_RED_LED, HIGH);
+        }
+    }
 }
